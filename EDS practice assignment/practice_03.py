e949bc5d8a58645ad741c7c3f46df5473d81ec40
sujalwankhade2008 <wankhadesujal2008@gmail.com>
2026-02-14 22:50:25 +0530

    Refactor age and salary conversion functions

--- EDS practice assignment/practice_03.py
+++ EDS practice assignment/practice_03.py
@@ -1,7 +1,16 @@
-def calculate_age(birth_year):
-    current_year = 2026
-    return current_year - birth_year
+from datetime import datetime
 
-def convert_salary(salary_in_usd):
-    exchange_rate = 81.50  # Example exchange rate
-    return salary_in_usd * exchange_rate
+def calculate_age(birthdate):
+    today = datetime.now()
+    birthdate = datetime.strptime(birthdate,"%Y-%m-%d")
+    return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+
+def salary_in_dollor(salary_in_rupees,conversion_rate= 90):
+    return salary_in_rupees / conversion_rate
+
+birthdate = input("Enter birthdate(YYYY-MM-DD): ")
+salary = float(input("Enter salary in rupees: "))
+age = calculate_age(birthdate)
+salary_usd = salary_in_dollor(salary)
+print(f"Age: {age} years")
+print(f"Salary in USD : ${salary_usd:.2f}")

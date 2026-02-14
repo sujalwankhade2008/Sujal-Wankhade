52d53f4b65512cdcac50dd81c6cdbe96da40a915
sujalwankhade2008 <wankhadesujal2008@gmail.com>
2026-02-14 22:49:40 +0530

    Refactor number reversal to use user input

--- EDS practice assignment/practice_04.py
+++ EDS practice assignment/practice_04.py
@@ -1,9 +1,3 @@
-# Function to reverse a number
-
-def reverse_number(num):
-    return int(str(num)[::-1])
-
-# Example usage
-if __name__ == '__main__':
-    number = 12345
-    print(f'The reverse of {number} is {reverse_number(number)}')
+number = int(input("Enter a number : "))
+reverse_num = int(str(number)[::-1])
+print(f"Reversed number: {reverse_num}")
